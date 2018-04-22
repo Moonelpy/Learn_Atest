3a128a5d1e7d74f0974dbdaa27d49e717e12b4b1
moonelpy <z_ivan_1987@mail.ru>
2018-04-23 00:45:59 +0300

    Дослушал 14 урок

--- sandbox/src/main/java/ru/stqa/Atest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/Atest/sandbox/MyFirstProgram.java
@@ -6,21 +6,22 @@ public class MyFirstProgram {
     hello("world!");
     hello("Ivan");
     hello("MOON");
-    double l = 5;
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    System.out.println("Площадь треугольника со сторонаями "+ a  + " И " + b + " = " + area(a, b));
+    Squere s = new Squere(5);
+
+    Rectangle r = new Rectangle(4,6);
+
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь треугольника со сторонаями "+ r.a + " И " + r.b + " = " + area(r));
   }
 
   public static void hello (String somebody) {
     System.out.println("Hello " + somebody + "!");
   }
-  public static double area(double len){
-    return len * len;
+  public static double area(Squere s) {
+    return s.l * s.l;
   }
-  public  static double area(double b, double a)
+  public  static double area(Rectangle r)
   {
-    return  a *  b;
+    return  r.a *  r.b;
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/Atest/sandbox/Squere.java
@@ -0,0 +1,8 @@
+package ru.stqa.Atest.sandbox;
+
+public class Squere {
+  public  double l;
+  public  Squere(double l) {
+    this.l = l;
+  }
+}
