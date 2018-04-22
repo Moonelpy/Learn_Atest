441c05f95f062bf0f50c551c684d20b0f777e730
moonelpy <z_ivan_1987@mail.ru>
2018-04-23 00:22:26 +0300

    Добавлены несколько функций

--- sandbox/src/main/java/ru/stqa/Atest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/Atest/sandbox/MyFirstProgram.java
@@ -1,8 +1,26 @@
-package ru.stqa.Atest.sandbox;
+ package ru.stqa.Atest.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] agrs) {
 
-    System.out.println("Hello World");
+    hello("world!");
+    hello("Ivan");
+    hello("MOON");
+    double l = 5;
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    System.out.println("Площадь треугольника со сторонаями "+ a  + " И " + b + " = " + area(a, b));
+  }
+
+  public static void hello (String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+  public static double area(double len){
+    return len * len;
+  }
+  public  static double area(double b, double a)
+  {
+    return  a *  b;
   }
 }
