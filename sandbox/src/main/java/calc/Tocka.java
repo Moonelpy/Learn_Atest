acfe41b0760792a0d48e430f212a516d6d631b2f
moonelpy <z_ivan_1987@mail.ru>
2018-04-25 00:45:26 +0300

    Кривоватый расчет(расстояния между двумя точками)

---
+++ sandbox/src/main/java/calc/Tocka.java
@@ -0,0 +1,21 @@
+package calc;
+
+public class Tocka {
+
+  public double x;
+  public double y;
+  public double p1;
+  public double p2;
+
+  double p1 (double x, double y) {
+    this.x = x;
+    this.y = y;
+return p1;
+  }
+
+  double p2 (double x, double y) {
+    this.x = x;
+    this.y = y;
+    return p2;
+  }
+}
